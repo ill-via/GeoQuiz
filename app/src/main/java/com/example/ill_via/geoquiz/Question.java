43c13e8e93f71efe4cf791f3a433d23ce49eb5b5
ill-via <ill_via@mail.ru>
2018-04-22 16:53:32 +0400

    first probe

---
+++ app/src/main/java/com/example/ill_via/geoquiz/Question.java
@@ -0,0 +1,27 @@
+package com.example.ill_via.geoquiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question (int textResId, boolean answerTrue){
+        mAnswerTrue = answerTrue;
+        mTextResId = textResId;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
